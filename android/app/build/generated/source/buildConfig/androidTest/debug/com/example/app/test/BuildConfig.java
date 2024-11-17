ab7c7ec1985cfc99a67eff206a3c780ad3baa141
alzemand <alzemand@outlook.com>
2024-11-17 10:58:04 -0300

    Adiciona configuração inicial do projeto Android, incluindo arquivos de manifesto, gradle e recursos, além de corrigir o nome do aplicativo para "Trava Letra"

--- android/app/build/generated/source/buildConfig/androidTest/debug/com/example/app/test/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/androidTest/debug/com/example/app/test/BuildConfig.java
@@ -5,6 +5,6 @@ package com.example.app.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.rumos.app.test";
+  public static final String APPLICATION_ID = "com.travaletras.app.test";
   public static final String BUILD_TYPE = "debug";
 }
--- android/app/build/generated/source/buildConfig/debug/com/example/app/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/example/app/BuildConfig.java
@@ -5,8 +5,8 @@ package com.example.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.rumos.app";
+  public static final String APPLICATION_ID = "com.travaletras.app";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
+  public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0";
 }
